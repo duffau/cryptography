f4203baa39d0edbafef64262453d4014f06a8a9a
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2020-03-08 18:38:41 +0100

    Renamed diffie hellman script

---
+++ key_exchange/diffie_hellman.py
@@ -0,0 +1,17 @@
+from random import getrandbits
+
+MODULUS = 13       # Large prime
+BASE = 7       # is primitive root modulo MODULUS
+KEY_BITS = 3   # The greatest possible integer should be less than MODULUS 
+BIT_FORMAT = '{:0%db}' % KEY_BITS
+
+
+def to_bits(integer):
+    return BIT_FORMAT.format(integer)
+
+def generate_secret(n_bits):
+    return getrandbits(n_bits)
+
+def calc_key(base, private_key, modulo):
+    return base**private_key % modulo
+
