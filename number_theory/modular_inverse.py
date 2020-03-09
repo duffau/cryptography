58ae94ae466bfeecd4e36a984299eb4545753d64
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2020-03-09 17:42:52 +0100

    Expanded modular_inverse tests.

--- number_theory/modular_inverse.py
+++ number_theory/modular_inverse.py
@@ -1,5 +1,6 @@
 from .gcd import eea
 
+
 def modular_inverse(n, modulo):
     _gcd, s, t = eea(modulo, n)
     if _gcd != 1:
--- tests/number_theory/modular_inverse_test.py
+++ tests/number_theory/modular_inverse_test.py
@@ -1,6 +1,17 @@
 from number_theory import modular_inverse
 
 
-def test_modular_inverse():
+def test_modular_inverse_coprimes():
     assert 13 * modular_inverse(13, 9) % 9 == 1
     assert 12 * modular_inverse(12, 67) % 67 == 1 
+    assert 2 * modular_inverse(2, 3) % 3 == 1
+    assert 3 * modular_inverse(3, 11) % 11 == 1
+    assert 7 * modular_inverse(7, 13) % 13 == 1
+
+
+def test_modular_inverse_non_coprimes():
+    assert modular_inverse(2, 2) is None
+    assert modular_inverse(2, 4) is None
+    assert modular_inverse(3, 6) is None
+    assert modular_inverse(3, 12) is None
+    assert modular_inverse(8, 64) is None
