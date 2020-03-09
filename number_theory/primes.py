5ccf92a2d509bf4906f1f625a10a57a7ebc4057f
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2020-03-09 12:15:30 +0100

    Added Sieve of Eratosthenes and test of primality

--- number_theory/primes.py
+++ number_theory/primes.py
@@ -23,3 +23,31 @@ def prime_factors(n):
     if n > 2: 
         factors.append(n)
     return factors
+
+
+def primes_less_than(upper_bound):
+    '''Generating primes using Sieves of Eratosthenes method'''
+    prime_indicator = [True for _ in range(upper_bound + 1)]
+
+    p = 2
+    while p*p <= upper_bound:
+        if prime_indicator[p]:
+            for i in range(p * 2, upper_bound + 1, p):
+                prime_indicator[i] = False
+        p += 1
+    return [i for i in range(2, upper_bound) if prime_indicator[i]]
+
+
+def is_prime(n):
+    if n <= 3:
+        return n > 1
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i*i <= n:
+        if n % i == 0 or n %(i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+
--- tests/number_theory/primes_test.py
+++ tests/number_theory/primes_test.py
@@ -1,4 +1,4 @@
-from number_theory.primes import prime_factors
+from number_theory.primes import prime_factors, primes_less_than, is_prime
 
 
 def product(numbers):
@@ -13,3 +13,23 @@ def test_prime_factors():
     for n in range(2, n_max):
         pfs = prime_factors(n)
         assert n == product(pfs), "{n} == product({pfs})".format(n=n, pfs=pfs)
+
+
+def test_is_prime():
+    primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 
+    59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 
+    137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 
+    211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271]
+    
+    non_primes = [n for n in range(1, max(primes)+1) if n not in set(primes)]
+
+    for prime_candidate in primes:
+        assert is_prime(prime_candidate)
+
+    for prime_candidate in non_primes:
+        assert not is_prime(prime_candidate)
+
+def test_primes_less_than():
+    upper_bound = 10**5
+    for prime_candidate in primes_less_than(upper_bound):
+        assert is_prime(prime_candidate)
