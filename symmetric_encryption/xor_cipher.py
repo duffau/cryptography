709588a6930ae1c765d955d8ef41ab621b00ce28
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2020-03-10 11:18:20 +0100

    Added modulus to keep the key smaller than the expected bit length.

--- symmetric_encryption/xor_cipher.py
+++ symmetric_encryption/xor_cipher.py
@@ -1,7 +1,7 @@
 
 def encrypt(message: str, key: int, bits: int = 16):
     n_bytes = bits // 8
-    key = key.to_bytes(n_bytes, 'big')
+    key = (key % 2 ** bits).to_bytes(n_bytes, 'big')
     cipher = bytearray()
     for i, message_byte in enumerate(message.encode('utf-8')):
         cipher.append(message_byte ^ key[i % n_bytes])
@@ -10,7 +10,7 @@ def encrypt(message: str, key: int, bits: int = 16):
 
 def decrypt(cipher: bytes, key: int, bits: int = 16):
     n_bytes = bits // 8
-    key = key.to_bytes(n_bytes, 'big')
+    key = (key % 2 ** bits).to_bytes(n_bytes, 'big')
     message = bytearray()
     for i, cipher_byte in enumerate(cipher):
         message.append(cipher_byte ^ key[i % n_bytes])
