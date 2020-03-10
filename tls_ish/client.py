4bddb0d0f86924f379c2846167b12a9fab35d796
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2020-03-10 22:39:54 +0100

    Exchanging encrypted text over TCP socket

--- tls_ish/client.py
+++ tls_ish/client.py
@@ -1,8 +1,15 @@
 import socket
+from symmetric_encryption.xor_cipher import encrypt
 
 HOST = "127.0.0.1"
 PORT = 8080
 ADDRESS = (HOST, PORT)
+SHARED_SECRET = 1234
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(ADDRESS)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(ADDRESS)
+    message = 'Hello'
+    cipher = encrypt(message, SHARED_SECRET)
+    sent = 0
+    while sent < len(cipher):
+        sent += s.send(cipher[sent:])
--- tls_ish/server.py
+++ tls_ish/server.py
@@ -1,13 +1,23 @@
 import socket
+from symmetric_encryption.xor_cipher import decrypt
 
 HOST = "127.0.0.1"
 PORT = 8080
 ADDRESS = (HOST, PORT)
+SHARED_SECRET = 1234
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(ADDRESS)
-serversocket.listen(5)
+serversocket.listen()
 
 while True:
     (clientsocket, address) = serversocket.accept()
-    print(clientsocket)
+    recieved_message = bytearray()
+    while True:
+        recvd = clientsocket.recv(2048)
+        if not recvd: break
+        recieved_message += recvd
+    print('recieved message: %s' % bytes(recieved_message))
+    print('decrypted message: %s' % decrypt(recieved_message, SHARED_SECRET))
+
+serversocket.close()
